9f1e143c5514916bfb94b75919a815e229b016b1
wuji <35483539+mypridelife@users.noreply.github.com>
2022-04-01 21:42:37 +0800

    feat: icon

--- src/components/ResultArea.vue
+++ src/components/ResultArea.vue
@@ -19,12 +19,36 @@ watch(sentenceFilter, () => {
   scrollTo(0)
 })
 
+// bounding
+const { x, y } = useMouse({ type: 'client' })
+const { element } = useElementByPoint({ x, y })
+const bounding = reactive(useElementBounding(element))
+useEventListener('scroll', bounding.update, true)
+
+const boxStyles = computed(() => {
+  if (element.value?.classList.contains('i-row')) {
+    return {
+      display: 'block',
+      width: `${bounding.width}px`,
+      height: `${bounding.height}px`,
+      left: `${bounding.left}px`,
+      top: `${bounding.top}px`,
+      backgroundColor: '#d7545566',
+      transition: 'all 0.2s linear',
+    } as Record<string, string | number>
+  }
+  return {
+    display: 'none',
+  }
+})
+
 </script>
 
 <template>
   <div>
+    <div :style="boxStyles" fixed pointer-events-none z-9999 border="1 #d7545599" />
     <div class="bg-trueGray-400 bg-opacity-25 w-full h-1px my-4" />
-    <div class="relative overflow-hidden h-full" flex="~ col" md="flex-row">
+    <div class="result-area relative overflow-hidden h-full" flex="~ col" md="flex-row">
       <div class="flex flex-col h-full w-full items-start">
         <div class="flex items-center py-4">
           <a i-carbon:text-annotation-toggle opacity50 mr-2 />
@@ -35,6 +59,7 @@ watch(sentenceFilter, () => {
             <div
               v-for="(s, i) in list"
               :key="i"
+              class="i-row"
               whitespace-pre-wrap
               break-all
               hyphens-auto
